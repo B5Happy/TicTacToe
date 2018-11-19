709cde24cdf3ea2f9d50a45658b34fb688a3cb9c
Saly <saly@juke4me.com>
2018-11-19 09:19:31 +0530

    Add computer play

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager: MonoBehaviour {
+
+    public string[] Data = new string[9];
+    [SerializeField] List<int> choice = new List<int>();
+
+    public void ConterPlay()
+    {
+        choice.Clear();
+
+        for(int i= 0; i < Data.Length; i++)
+        {
+            if (Data[i] == string.Empty)
+            {
+                choice.Add(i);
+            }
+        }
+
+        int button = choice[Random.Range(0, choice.Count)];
+
+        Button btn = GameObject.Find(button.ToString()).GetComponent<Button>();
+
+        btn.interactable = false;
+
+        btn.GetComponentInChildren<Text>().text = "O";
+
+        Data[button] = "O";
+
+        if (IsWin("O"))
+        {
+            Debug.Log("O win");
+            return;
+        }
+    }
+
+    public bool IsWin(string s)
+    {
+
+        if (Data[0] == s && Data[1] == s && Data[2] == s ||
+           Data[3] == s && Data[4] == s && Data[5] == s ||
+           Data[6] == s && Data[7] == s && Data[8] == s ||
+
+           Data[0] == s && Data[3] == s && Data[6] == s ||
+           Data[1] == s && Data[4] == s && Data[7] == s ||
+           Data[2] == s && Data[5] == s && Data[8] == s ||
+
+           Data[0] == s && Data[4] == s && Data[9] == s ||
+           Data[2] == s && Data[4] == s && Data[6] == s)
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ Assets/Script/SelectBox.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectBox : MonoBehaviour {
+
+    GameManager gm;
+
+    private void Start()
+    {
+        gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
+    public void WriteX()
+    {
+        GetComponentInChildren<Text>().text = "X";
+        GetComponent<Button>().interactable = false;
+
+        gm.Data[int.Parse(gameObject.name)] = "X";
+
+        
+        if (gm.IsWin("X"))
+        {
+            Debug.Log("X win");
+            return;
+        }
+        else
+        {
+            gm.ConterPlay();
+        }
+    }
+}
