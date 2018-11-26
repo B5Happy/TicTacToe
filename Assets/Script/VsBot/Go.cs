94312f3282fcf4dbdd4ad96fbe6fc196554179b7
Saly <saly@juke4me.com>
2018-11-26 23:11:13 +0530

    Add message at the end of the game

--- Assets/Script/VsBot/Go.cs
+++ Assets/Script/VsBot/Go.cs
@@ -2,18 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Go : MonoBehaviour {
 
     GameManager gm;
 
+
+    private void Start()
+    {
+
+        gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+
+
+        // Write a message at each case
+        if (gm.IsWin("O"))
+        {
+            GameObject.Find("Result").GetComponent<Text>().text = "Axe win! Click to take revenge!";
+
+        }
+        else if (gm.IsWin("X"))
+        {
+
+            GameObject.Find("Result").GetComponent<Text>().text = "You win! Click to play again.";
+
+        }
+        else
+        {
+            GameObject.Find("Result").GetComponent<Text>().text = "The game is hard! Click to get more power.";
+        }
+
+        Update();
+    }
+
     private void Update()
     {
+      
         //Press any key to restart the game
         if (Input.anyKey)
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-            //gm.Restart();
+            
         }
     }
 }
--- Assets/Script/VsFriend/Go2.cs
+++ Assets/Script/VsFriend/Go2.cs
@@ -2,8 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Go2 : MonoBehaviour {
+
+
+    ManagerVF rofl;
+
+
+    private void Start()
+    {
+
+        rofl = GameObject.Find("ManagerVF").GetComponent<ManagerVF>();
+
+
+        // Write a message at each case
+        if (rofl.IsWin("O"))
+        {
+            GameObject.Find("Result").GetComponent<Text>().text = "Your friend win! Click to take revenge!";
+
+        }
+        else if (rofl.IsWin("X"))
+        {
+
+            GameObject.Find("Result").GetComponent<Text>().text = "Ez win! Click to smak again.";
+
+        }
+        else
+        {
+            GameObject.Find("Result").GetComponent<Text>().text = "The game is hard! Click to get more chance.";
+        }
+
+        Update();
+    }
 
     private void Update()
     {
